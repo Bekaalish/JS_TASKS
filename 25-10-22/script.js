f8ea18c388f6b91ddf32db4f625328b69280c004
Alishov <beka.alishov@gmail.com>
2022-10-25 11:04:33 +0600

    feat:added js

---
+++ 25-10-22/script.js
@@ -0,0 +1,14 @@
+let a = 17;
+let b = 10;
+console.log(a - b);
+
+let h = 'час';
+let m = "минута";
+let s = `секунда`
+let hours = `${h}:${m}:${s}`;
+console.log(hours);
+
+let num = 10;
+let num1 = num++;
+let num2 = num++;
+alert(num);
